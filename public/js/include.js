12b61feab619c04d3aef54341f9d8db892f3e319
ariados22 <ariados22@gmail.com>
2023-01-12 17:00:23 +0900

    230112 목 1700

--- public/js/include.js
+++ public/js/include.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $('#header').load('./include/header.html');
   $('#footer').load('./include/footer.html');
   $('#aside').load('./include/aside.html');
+  $('#buildHeader').load('../include/header.html');
+  $('#buildFooter').load('../include/footer.html');
+  $('#buildAside').load('../include/aside.html');
 });
